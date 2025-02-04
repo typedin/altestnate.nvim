5cfffc39fe8d1e98ea4a496874f50257fe07650c
typedin <antoine@typedin.dev>
2025-02-04 20:40:54 +0100

    valid json is written to projection file

---
+++ lua/altestnate/utils/input_to_json_string.lua
@@ -0,0 +1,26 @@
+local create_projections_entry = require("altestnate.commands.create_projections_entry")
+local json = require("altestnate.utils.json")
+
+local function M(input)
+  local args = {
+    "entry_key",
+    "test_folder",
+    "test_suffix",
+  }
+
+  local mapped_args = {}
+
+  local index = 1
+  for word in string.gmatch(input, "%S+") do
+    local key = args[index]
+    if key then
+      mapped_args[key] = word
+    end
+    index = index + 1
+  end
+  local tableWithData = create_projections_entry(mapped_args)
+
+  return json.stringify(tableWithData)
+end
+
+return M
