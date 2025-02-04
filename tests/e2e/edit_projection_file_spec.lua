710f27bdb940ac4308dd2c379e786109634b4ce1
typedin <antoine@typedin.dev>
2025-02-04 18:58:22 +0100

    cleaned up test so it runs with minit

--- tests/e2e/edit_projection_file_spec.lua
+++ tests/e2e/edit_projection_file_spec.lua
@@ -1,21 +1,17 @@
-local test_util = require("tests.test_util")
+local projections_file = vim.fn.expand(".test_projections_file.json")
+require("altestnate").setup({
+  projections_file = projections_file,
+})
 
 describe("Edit project file", function()
-  after_each(function()
-    test_util.reset_editor()
-  end)
-
-  local projections_file = vim.fn.expand(".test_projections_file.json")
-
   before_each(function()
-    os.execute("touch " .. projections_file)
     vim.wait(1000, function()
+      os.execute("touch " .. projections_file)
       return vim.fn.filereadable(projections_file) == 1
     end, 50)
   end)
 
   after_each(function()
-    test_util.reset_editor()
     os.execute("rm -rf " .. projections_file)
   end)
 
@@ -25,16 +21,13 @@ describe("Edit project file", function()
 
     -- Schedule the setup and file opening
     vim.schedule(function()
-      require("altestnate").setup({
-        projections_file = projections_file,
-      })
       require("altestnate.fs").edit_projection(projections_file)
     end)
 
     -- Wait for the buffer to open (this will ensure the file is loaded)
-    vim.wait(500, function()
+    vim.wait(250, function()
       return vim.fn.bufloaded(vim.fn.bufname(0)) == 1
-    end, 50)
+    end, 10)
 
     -- Sanity check: Feed keys to the prompt after the file is loaded
     vim.api.nvim_feedkeys("y\n", "n", true)
