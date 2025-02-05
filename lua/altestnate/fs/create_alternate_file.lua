bacac4df22feec7f53c930ecc9b8b7bd20193f8c
typedin <antoine@typedin.dev>
2025-02-05 14:26:19 +0100

    create_alternate_file is a unit

---
+++ lua/altestnate/fs/create_alternate_file.lua
@@ -0,0 +1,14 @@
+--- Create a new file at the given path
+--- @param file_path string
+--- @return number -1 if the file already exists or the operation fails, 0 otherwise
+local function create_alternate_file(file_path)
+  local dir = vim.fn.fnamemodify(file_path, ":h") -- Get the directory of the new file
+  -- p will silently exit if the directory already exists
+  vim.fn.mkdir(dir, "p") -- Create the directory if it doesn't exist
+  if vim.fn.filereadable(file_path) == 1 then
+    return -1
+  end
+  return vim.fn.writefile({}, file_path)
+end
+
+return create_alternate_file
