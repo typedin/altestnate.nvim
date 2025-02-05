837fa86197237c87b33afb6271ca8b2c5445f080
typedin <antoine@typedin.dev>
2025-02-05 23:32:22 +0100

    wip: refactoring

--- tests/integration/add_projection_spec.lua
+++ tests/integration/add_projection_spec.lua
@@ -4,6 +4,16 @@ require("altestnate").setup({
   projections_file = projections_file,
 })
 
+---@param args table
+local function feedkeys(args)
+  -- Example of a source file (ex: src/init.lua):
+  vim.api.nvim_feedkeys(args.step1 .. "\n", "n", true)
+  -- Test folder for this source f .. "\n",le (ex: tests):
+  vim.api.nvim_feedkeys(args.step2 .. "\n", "n", true)
+  -- Suffix for the source file (e .. "\n",: _spec)
+  vim.api.nvim_feedkeys(args.step3 .. "\n", "n", true)
+end
+
 describe("A user can populate their own projection file by interacting with the plugin", function()
   after_each(function()
     os.execute("rm -rf " .. projections_file)
@@ -14,8 +24,7 @@ describe("A user can populate their own projection file by interacting with the 
       vim.api.nvim_command("AddProjection")
     end)
 
-    -- Enter choices (space-separated):
-    vim.api.nvim_feedkeys("src/init.lua tests _spec\n", "n", true)
+    feedkeys({ step1 = "src/init.lua", step2 = "tests", step3 = "_spec" })
     -- Wait for the file to be written
     vim.wait(500, function()
       return vim.fn.filereadable(projections_file) == 1
@@ -26,13 +35,44 @@ describe("A user can populate their own projection file by interacting with the 
     assert(vim.deep_equal(parsed_content, expected_content), "File content does not match")
   end)
 
-  it("doesn't create anything when the user aborts", function()
+  it("doesn't create anything when the user aborts at first step", function()
     vim.schedule(function()
       vim.api.nvim_command("AddProjection")
     end)
 
-    -- Create a projections file? (y/n):
-    vim.api.nvim_feedkeys("\n", "n", true)
+    feedkeys({ step1 = "", step2 = "tests", step3 = "_spec" })
+    -- Wait for the file to be written
+    vim.wait(500, function()
+      return vim.fn.filereadable(projections_file) == 1
+    end, 50)
+
+    assert.is_false(vim.fn.filereadable(projections_file) == 1)
+  end)
+
+  it("doesn't create anything when the user aborts at second step", function()
+    vim.schedule(function()
+      vim.api.nvim_command("AddProjection")
+    end)
+
+    feedkeys({ step1 = "src/init.lua", step2 = "", step3 = "_spec" })
+    -- Wait for the file to be written
+    vim.wait(500, function()
+      return vim.fn.filereadable(projections_file) == 1
+    end, 50)
+
+    assert.is_false(vim.fn.filereadable(projections_file) == 1)
+  end)
+
+  it("doesn't create anything when the user aborts at third step", function()
+    vim.schedule(function()
+      vim.api.nvim_command("AddProjection")
+    end)
+
+    feedkeys({ step1 = "src/init.lua", step2 = "tests", step3 = "" })
+    -- Wait for the file to be written
+    vim.wait(500, function()
+      return vim.fn.filereadable(projections_file) == 1
+    end, 50)
 
     assert.is_false(vim.fn.filereadable(projections_file) == 1)
   end)
@@ -44,9 +84,7 @@ describe("A user can populate their own projection file by interacting with the 
       vim.api.nvim_command("AddProjection")
     end)
     --
-    -- Enter choices (space-separated):
-    vim.api.nvim_feedkeys("lua/init.lua spec _test\n", "n", true)
-
+    feedkeys({ step1 = "lua/init.lua", step2 = "spec", step3 = "_test" })
     -- Wait for the file to be written
     vim.wait(500, function()
       return vim.fn.filereadable(projections_file) == 1
@@ -63,10 +101,8 @@ describe("A user can populate their own projection file by interacting with the 
       os.execute("touch " .. projections_file)
       vim.api.nvim_command("AddProjection")
     end)
-    --
-    -- Enter choices (space-separated):
-    vim.api.nvim_feedkeys("src/init.lua tests _spec\n", "n", true)
 
+    feedkeys({ step1 = "src/init.lua", step2 = "tests", step3 = "_spec" })
     -- Wait for the file to be written
     vim.wait(500, function()
       return vim.fn.filereadable(projections_file) == 1
